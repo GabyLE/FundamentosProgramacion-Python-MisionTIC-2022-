33498806e08d7e3641d548eed16a8e285ec2dc02
GabyLE <denis.lopez@correounivalle.edu.co>
2021-06-05 18:44:40 -0500

    punto 3 taller 5

--- FundamentosProgramacion/Semana-3/taller-5.py
+++ FundamentosProgramacion/Semana-3/taller-5.py
@@ -1,13 +1,43 @@
-from tkinter import *
-import random
+# ENTRADA
+bin = input("Ingrese número decimal: ")
 
-v = Tk()
-v.title("Apuntado")
-v.minsize(width= 300, height= 200)
-v.configure(background= 'green')
+# PROCESO
+# variables
+grupos =[]
+decimales = []
+hex = ""
 
-# Button
-Button(v, text= "Repartir", command= repartir).grid(row=0, column=0)
-Button(v, text="Chequear", command=verificar).grid(row=0, column=1)
+# constantes
+hexadecimal = {10:"A", 11:"B",12:"C", 13:"D", 14:"E", 15:"F"}
 
-v.mainloop()
+# Calculo cifras sobrantes al agrupar el número en grupos de 4
+sobra = len(bin) % 4
+# añade el valor agrupado sobrante a la lista grupos
+grupos.append(bin[:sobra])
+
+# añade los grupos de cuatro digitos a la lista grupos
+lim_s= sobra + 4
+for i in range(sobra,len(bin),4):
+    grupos.append(bin[i:lim_s])
+    lim_s += 4
+
+# Calculo de binario a decimal con los valores agrupados
+for str in grupos:
+    pow = 0
+    dec = 0
+    for i in range(len(str)-1, -1, -1):
+        num = int(str[i])
+        dec = dec + (2**pow)*num
+        pow += 1
+    decimales.append(dec)
+
+# Decimal a hexadecimal
+for decimal in decimales:
+    
+    if decimal < 10:
+        hex += "{}".format(decimal)
+    else:
+        hex += hexadecimal[decimal]
+
+# SALIDA
+print("Hexadecimal: " + hex)
