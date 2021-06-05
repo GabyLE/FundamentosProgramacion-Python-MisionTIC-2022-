bda16e158e5e2dcf12b76ea842c51a63a1a399a5
GabyLE <denis.lopez@correounivalle.edu.co>
2021-06-05 15:32:14 -0500

    cambio 2

--- FundamentosProgramacion/Semana-3/taller-5.py
+++ FundamentosProgramacion/Semana-3/taller-5.py
@@ -3,5 +3,11 @@ import random
 
 v = Tk()
 v.title("Apuntado")
+v.minsize(width= 300, height= 200)
+v.configure(background= 'green')
 
-# Label
+# Button
+Button(v, text= "Repartir", command= repartir).grid(row=0, column=0)
+Button(v, text="Chequear", command=verificar).grid(row=0, column=1)
+
+v.mainloop()
