c96dc5db367665518bb8dedd22b8c5a95ed97d28
GabyLE <denis.lopez@correounivalle.edu.co>
2021-06-06 12:42:44 -0500

    programa principal juego dados

--- FundamentosProgramacion/Semana-3/Dados/Dados.py
+++ FundamentosProgramacion/Semana-3/Dados/Dados.py
@@ -4,15 +4,16 @@ import random
 class Dado:
     # Método constructor
     def __init__(varClase):
+        # Inicia los dados en 1
         varClase.numero = 1
 
     def lanzar(varClase):
-        '''Generar aleatoriamente el dado'''
+        '''Generar aleatoriamente el número del dado'''
         varClase.numero = random.randrange(1,7)
 
     def mostrar(varClase, lblDado):
         # Cargar la imagen
-        imgDado = PhotoImage(file= = "./" + str(varClase.numero) + ".png")
+        imgDado = PhotoImage(file= "./" + str(varClase.numero) + ".png")
         # Mostrar imagen
         lblDado.config(image = imgDado)
         lblDado.image = imgDado
--- FundamentosProgramacion/Semana-3/Dados/JuegoDados.py
+++ FundamentosProgramacion/Semana-3/Dados/JuegoDados.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import Util
+from Dados import Dado
 
 #crear ventana
 v = Tk()
@@ -17,10 +18,48 @@ Label(v, text="Cenas", font= "Consolas 10",  fg= 'white', bg='#0B3442').grid(row
 txtLanzamientos=Util.agregarCajaTextoSalida(v, 3, 3, 0, "Arial 48 bold")
 txtCenas=Util.agregarCajaTextoSalida(v, 3, 3, 1, "Arial 48 bold")
 
+# variables globales
+lanzamientos = 0
+cenas = 0
 
-Button(v, text="Lanzar", font= "Consolas 10").grid(row=1, column=0)
-Button(v, text= "Iniciar", font= "Consolas 10").grid(row= 1, column= 1)
+# Instancias de Dado
+d1 = Dado()
+d2 = Dado()
+
+# MÉTODOS
+def iniciar():
+    global lanzamientos, cenas
+    lanzamientos = 0
+    cenas = 0
+
+    # Mostrar los contadores
+    Util.mostrarCajaTexto(txtLanzamientos, lanzamientos)
+    Util.mostrarCajaTexto(txtCenas, cenas)
+    btnL.configure(state= NORMAL)
 
 
+def lanzar():
+    global lanzamientos, cenas
+    # Lazar los dados y mostrar la imagen de los dades
+    d1.lanzar()
+    d1.mostrar(lblDado1)
+    d2.lanzar()
+    d2.mostrar(lblDado2)
+
+    # Actualizar contador lanzaminetos
+    lanzamientos += 1
+
+    # contador cenas
+    if d1.obtenerNumero() + d2.obtenerNumero() >= 11:
+        cenas += 1
+
+    # Mostrar los contadores
+    Util.mostrarCajaTexto(txtLanzamientos, lanzamientos)
+    Util.mostrarCajaTexto(txtCenas, cenas)
+
+btnL = Button(v, text="Lanzar", font= "Consolas 10", command= lanzar)
+btnL.grid(row=1, column=0)
+btnL.configure(state= DISABLED)
+Button(v, text= "Iniciar", font= "Consolas 10", command= iniciar).grid(row= 1, column= 1)
 
 v.mainloop()
